8f707589f711d6924c2248b5a27de3f72c5fc5c3
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2001-10-23 17:09:12 +0000

    An object placed "off-center" will now be correctly tapered.
    Twist have Shift/Ctrl constraints at the same rotations as Rotate.

--- src/wings_deform.erl
+++ src/wings_deform.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_deform.erl,v 1.10 2001/10/03 09:24:11 bjorng Exp $
+%%     $Id: wings_deform.erl,v 1.11 2001/10/23 17:09:12 bjorng Exp $
 %%
 
 -module(wings_deform).
@@ -104,10 +104,12 @@ taper(Primary, Effect, St) ->
     wings_drag:init_drag(Tvs, {-1.0,?HUGE}, St).
 
 taper(#shape{id=Id,sh=We}, Vs0, Primary, Effect, Acc) ->
+    [MinR,MaxR] = wings_vertex:bounding_box(Vs0, We),
     Key = key(Primary),
-    {Min,Max} = range(Key, We),
-    Range = Max-Min,
-    Tf = taper_fun(Primary, Effect),
+    Min = element(Key, MinR),
+    Max = element(Key, MaxR),
+    Range = Max - Min,
+    Tf = taper_fun(Key, Effect, MinR, MaxR),
     Vs = gb_sets:to_list(Vs0),
     Fun = fun(#shape{sh=#we{vs=Vtab0}=W}=Sh, Dx, Dy, St) ->
 		  U = Dx + 1.0,
@@ -115,43 +117,44 @@ taper(#shape{id=Id,sh=We}, Vs0, Primary, Effect, Acc) ->
 			 fun(V, Vt) ->
 				 Rec = gb_trees:get(V, Vt),
 				 #vtx{pos=Pos0} = Rec,
-				 Pos = Tf(U, Min, Range, Pos0),
+				 Pos = Tf(U, Pos0),
 				 gb_trees:update(V, Rec#vtx{pos=Pos}, Vt)
 			 end, Vtab0, Vs),
 		  {shape,Sh#shape{sh=W#we{vs=Vt}}}
 	  end,
     [{Id,Fun}|Acc].
 
-taper_fun(x, yz) ->
-    fun(U, Min, Range, {X,Y,Z})
-       when float(U), float(Min), float(Range), float(X), float(Y), float(Z) ->
-	    S0 = (X-Min)/Range,
-	    S = mix(S0, U),
-	    {X,Y*S,Z*S}
-    end;
-taper_fun(y, xz) ->
-    fun(U, Min, Range, {X,Y,Z})
-       when float(U), float(Min), float(Range), float(X), float(Y), float(Z) ->
-	    S0 = (Y-Min)/Range,
-	    S = mix(S0, U),
-	    {X*S,Y,Z*S}
-    end;
-taper_fun(z, xy) ->
-    fun(U, Min, Range, {X,Y,Z})
-       when float(U), float(Min), float(Range), float(X), float(Y), float(Z) ->
-	    S0 = (Z-Min)/Range,
-	    S = mix(S0, U),
-	    {X*S,Y*S,Z}
-    end;
-taper_fun(Primary, Effect0) ->
-    Key = key(Primary),
-    Effect = key(Effect0),
-    fun(U, Min, Range, Pos) when float(U), float(Min), float(Range) ->
-	    S0 = (element(Key, Pos)-Min)/Range,
-	    S = mix(S0, U),
-	    setelement(Effect, Pos, element(Effect, Pos)*S)
+taper_fun(Key, Effect, {IX,IY,IZ}=MinR, {AX,AY,AZ}=MaxR) ->
+    Min = element(Key, MinR),
+    Range = element(Key, MaxR) - element(Key, MinR),
+    {Ekey1,Ekey2} = effect(Effect),
+    io:format("~w\n", [{Ekey1,Ekey2}]),
+    Eoffset1 = (element(Ekey1, MinR)+element(Ekey1, MaxR))/2,
+    case Ekey2 of
+	none ->
+	    fun(U, Pos) when float(U), float(Min), float(Range) ->
+		    S0 = (element(Key, Pos)-Min)/Range,
+		    S = mix(S0, U),
+		    E = S * (element(Ekey1, Pos)-Eoffset1) + Eoffset1,
+		    setelement(Ekey1, Pos, E)
+	    end;
+	Other ->
+	    Eoffset2 = (element(Ekey2, MinR)+element(Ekey2, MaxR))/2,
+	    fun(U, Pos0) when float(U), float(Min), float(Range) ->
+		    S0 = (element(Key, Pos0)-Min)/Range,
+		    S = mix(S0, U),
+		    E0 = S * (element(Ekey1, Pos0)-Eoffset1) + Eoffset1,
+		    E =  S * (element(Ekey2, Pos0)-Eoffset2) + Eoffset2,
+		    Pos = setelement(Ekey1, Pos0, E0),
+		    setelement(Ekey2, Pos, E)
+	    end
     end.
-    
+
+effect(yz) -> {2,3};
+effect(xy) -> {1,2};
+effect(xz) -> {1,3};
+effect(Single) -> {key(Single),none}.
+
 mix(A, F) ->
     F + (1-F)*A.
     
@@ -172,7 +175,11 @@ twist(#shape{id=Id,sh=We}, Vs0, Axis, Acc) ->
     Range = Max-Min,
     Vs = gb_sets:to_list(Vs0),
     Fun = fun(#shape{sh=#we{vs=Vtab0}=W}=Sh, Dx, Dy, St) ->
-		  U = (Dx * 2.0 * ?PI)/Range,
+		  Angle = Dx * 15,
+		  U = (Angle / 180.0 * ?PI)/Range,
+		  wings_io:message(lists:flatten(
+				     io_lib:format("A:~10p",
+						   [Angle]))),
 		  Vt = foldl(
 			 fun(V, Vt) ->
 				 Rec = gb_trees:get(V, Vt),
