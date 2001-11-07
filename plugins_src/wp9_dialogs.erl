a75e7d385594e8ac9f2593ec35ed408491453919
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2001-11-07 07:11:55 +0000

    Extended ask "syntax".

--- plugins_src/wp9_dialogs.erl
+++ plugins_src/wp9_dialogs.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wp9_dialogs.erl,v 1.2 2001/10/25 14:24:04 bjorng Exp $
+%%     $Id: wp9_dialogs.erl,v 1.3 2001/11/07 07:11:55 bjorng Exp $
 %%
 
 -module(wp9_dialogs).
@@ -49,7 +49,7 @@ ui({ask,Qs}, Next) ->
 ui(What, Next) -> Next(What).
 
 
-ask([{Prompt,Default,Min,Max}|T]=T0) ->
+ask([{Prompt,Default,Min,Max}|T]=T0) when is_integer(Default) ->
     case wings_getline:number(Prompt ++ ": ", Default) of
 	aborted -> aborted;
 	N ->
@@ -58,4 +58,33 @@ ask([{Prompt,Default,Min,Max}|T]=T0) ->
 		Ns -> [N|Ns]
 	    end
     end;
+ask([{Prompt,Def}|T]=T0) ->
+    Str0 = lists:flatten(io_lib:format("~p", [Def])),
+    case wings_getline:string(Prompt ++ ": ", Str0) of
+	aborted -> aborted;
+	Str ->
+	    case catch make_term(Str) of
+		error -> ask(T0);
+		N ->
+		    case ask(T) of
+			aborted -> ask(T0);
+			Ns -> [N|Ns]
+		    end
+	    end
+    end;
 ask([]) -> [].
+
+make_term(Str) ->
+    case erl_scan:string(Str) of
+	{ok, Tokens, _} ->
+	    case erl_parse:parse_term(Tokens ++ [{dot, 1}]) of
+		{ok, Term} -> Term;
+		{error, {_,_,Reason}} ->
+		    io:format("~s: ~s~n", [Reason, Str]),
+		    throw(error)
+	    end;
+	{error, {_,_,Reason}, _} ->
+	    io:format("~s: ~s~n", [Reason, Str]),
+	    throw(error)
+    end.
+
