e9474bb1e03f7a62436d202bc2fe5e1144ace80a
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-01-17 09:27:29 +0000

    Object creation plug-ins can now return an e3d_object record (first draft).

--- src/wings_plugin.erl
+++ src/wings_plugin.erl
@@ -8,12 +8,13 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_plugin.erl,v 1.6 2001/12/29 20:33:56 bjorng Exp $
+%%     $Id: wings_plugin.erl,v 1.7 2002/01/17 09:27:29 bjorng Exp $
 %%
 -module(wings_plugin).
 -export([init/0,menu/2,command/2,call_ui/1]).
 
 -include("wings.hrl").
+-include("e3d.hrl").
 -import(lists, [append/1,flatmap/2,foreach/2,sort/1,reverse/1,foldl/3]).
 
 %%%
@@ -168,6 +169,9 @@ command(creator, Mod, Cmd, Ask, St) ->
 command(unsupported, Mod, Cmd, Ask, St) ->
     check_result(Mod:command(Cmd, Ask, St), St).
 
+check_result({new_shape,Prefix,#e3d_object{}=Obj,Mat}, St0) ->
+    {UsedMat,St} = import_object(Obj, St0),
+    add_materials(UsedMat, Mat, St);
 check_result({new_shape,Prefix,Fs,Vs}, #st{onext=Oid}=St) ->
     We = wings_we:build(Fs, Vs),
     Name = Prefix++integer_to_list(Oid),
@@ -176,3 +180,66 @@ check_result(aborted, St) -> aborted;
 check_result({drag,_}=Drag, _) -> Drag;
 check_result(#st{}=St, _) -> St.
 
+import_object(Obj, St) ->
+    import_object(Obj, gb_sets:empty(), St).
+
+import_object(#e3d_object{name=Name,obj=Obj}, UsedMat0, St0) ->
+    #e3d_mesh{matrix=Matrix,vs=Vs0,tx=Tx0,fs=Fs0,he=He} = Obj,
+    io:format("Name ~p\n", [Name]),
+    Vs = scale(Vs0, Matrix),
+    ObjType = obj_type(Tx0),
+    io:format("~w\n", [ObjType]),
+    Tx = list_to_tuple(Tx0),
+    {Fs,UsedMat} = import_faces(Fs0, Tx, [], UsedMat0),
+    St = build_object(Name, ObjType, Fs, Vs, He, St0),
+    {UsedMat,St}.
+
+import_faces([#e3d_face{vs=Vs,tx=Tx0,mat=Mat0}|Fs], Txs, Acc, UsedMat0) ->
+    UsedMat = add_used_mat(Mat0, UsedMat0),
+    Mat = translate_mat(Mat0),
+    FaceData = case Tx0 of
+		   [] -> {Mat,Vs};
+		   Tx1 ->
+		       Tx = [element(Tx+1, Txs) || Tx <- Tx1],
+		       {Mat,Vs,Tx}
+	       end,
+    import_faces(Fs, Txs, [FaceData|Acc], UsedMat);
+import_faces([], Txs, Acc, UsedMat) -> {reverse(Acc),UsedMat}.
+
+scale(Vs, none) -> Vs;
+scale(Vs, Matrix) -> [e3d_mat:mul_point(Matrix, P) || P <- Vs].
+
+obj_type([]) -> material;
+obj_type([{_,_}|_]) -> uv;
+obj_type([{_,_,_}|_]) -> vertex.
+
+add_used_mat([], UsedMat) -> UsedMat;
+add_used_mat([M|Ms], UsedMat) -> add_used_mat(Ms, gb_sets:add(M, UsedMat)).
+    
+translate_mat([]) -> default;
+translate_mat([Mat]) -> Mat;
+translate_mat([_|_]=List) -> List.
+
+build_object(Name, Type, Fs, Vs, He, St) ->
+    case catch wings_we:build(Type, Fs, Vs, He) of
+	{'EXIT',Reason} ->
+	    io:format("Conversion failed: ~P\n", [Reason,20]),
+	    St;
+	We -> store_object(Name, We, St)
+    end.
+
+store_object(undefined, We, #st{onext=Oid}=St) ->
+    Name = "unnamed_object" ++ integer_to_list(Oid),
+    wings_shape:new(Name, We, St);
+store_object(Name, We, St) ->
+    wings_shape:new(Name, We, St).
+
+add_materials(UsedMat0, Mat0, St) ->
+    UsedMat = sofs:from_external(gb_sets:to_list(UsedMat0), [name]),
+    Mat1 = sofs:relation(Mat0, [{name,data}]),
+    Mat2 = sofs:restriction(Mat1, UsedMat),
+    NotDefined0 = sofs:difference(UsedMat, sofs:domain(Mat2)),
+    DummyData = sofs:from_term([], data),
+    NotDefined = sofs:constant_function(NotDefined0, DummyData),
+    Mat = sofs:to_external(sofs:union(Mat2, NotDefined)),
+    wings_material:add_materials(Mat, St).
