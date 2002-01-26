b85fac665990132fb931b05f36fe0bfb7fe8fd4c
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-01-26 11:21:05 +0000

    Changed the plug-in interface to allow plug-ins to add entries to any menu and to have hotkeys. Changes are NOT backward-compatible. Old plug-ins will NOT work. (Exception: User-interface plug-ins, wp8* and wp9*, will will still work.)

--- src/wings_plugin.erl
+++ src/wings_plugin.erl
@@ -3,12 +3,12 @@
 %%
 %%     Experimental support of plugins.
 %%
-%%  Copyright (c) 2001 Jakob Cederlund, Bjorn Gustavsson
+%%  Copyright (c) 2001-2002 Jakob Cederlund, Bjorn Gustavsson
 %%
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_plugin.erl,v 1.9 2002/01/18 10:43:05 bjorng Exp $
+%%     $Id: wings_plugin.erl,v 1.10 2002/01/26 11:21:05 bjorng Exp $
 %%
 -module(wings_plugin).
 -export([init/0,menu/2,command/2,call_ui/1]).
@@ -41,25 +41,74 @@
 %%%
 
 init() ->
-    Dict = case try_dirs() of
-	       none -> [];
-	       PluginDir -> init(PluginDir)
-	   end,
-    put(wings_plugins, gb_trees:from_orddict(Dict)).
+    put(wings_plugins, []),
+    put(wings_ui, def_ui_plugin()),
+    case try_dirs() of
+	none -> ok;
+	PluginDir -> init(PluginDir)
+    end.
+
+call_ui(What) ->
+    Ui = get(wings_ui),
+    Ui(What).
+
+menu(Name, Menu) ->
+    menu_1(get(wings_plugins), Name, Menu).
+
+menu_1([{Type,M}|Ps], Name, Menu0) ->
+    case catch M:menu(Name, Menu0) of
+	Menu when is_list(Menu) ->
+	    menu_1(Ps, Name, Menu);
+	Other ->
+	    io:format("~w:menu/2: bad return value: ~P\n", [M,Other,20]),
+	    menu_1(Ps, Name, Menu0)
+    end;
+menu_1([], Name, Menu) -> Menu.
+
+command(Cmd, St) ->
+    command(get(wings_plugins), Cmd, St).
+
+command([{Type,M}|Ps], Cmd, St) ->
+    case catch M:command(Cmd, false, St) of
+	next -> command(Ps, Cmd, St);
+	Other ->
+	    case check_result(M, Other, St) of
+		next -> command(Ps, Cmd, St);
+		Res -> Res
+	    end
+    end;
+command([], Cmd, St) -> next.
+
+%%%
+%%% Local functions.
+%%%
 
 init(Dir) ->
     {Pas,Beams} = list_dir(Dir),
     foreach(fun(Pa) -> code:add_patha(Pa) end, Pas),
     TypeMods0 = [to_module(Beam) || Beam <- Beams],
-    TypeMods = load_modules(TypeMods0),
-    UiMods = reverse(sort([Mod || {user_interface,Mod} <- TypeMods])),
-    init_ui_plugins(UiMods),
-    Menus0 = [rearrange(N, T, Type, M, C) ||
-		 {Type,M} <- TypeMods, {N,{T,C}} <- catch M:menus()],
-    Menus1 = sofs:relation(Menus0),
-    Menus = sofs:relation_to_family(Menus1),
-    sofs:to_external(Menus).
+    TypeMods = reverse(sort(load_modules(TypeMods0))),
+    put(wings_plugins, init_plugins(TypeMods)).
 
+init_plugins([{user_interface,M}|T]) ->
+    Ui0 = get(wings_ui),
+    case catch M:init(Ui0) of
+	Ui when is_function(Ui) ->
+	    put(wings_ui, Ui);
+	Other ->
+	    io:format("~w:init/1 bad return value: ~P\n", [M,Other,20])
+    end,
+    init_plugins(T);
+init_plugins([{Type,M}=TypeMod|T]) ->
+    case catch M:init() of
+	true -> [TypeMod|init_plugins(T)];
+	false -> init_plugins(T);
+	Other ->
+	    io:format("~w:init/0 bad return value: ~P\n", [M,Other,20]),
+	    init_plugins(T)
+    end;
+init_plugins([]) -> [].
+    
 load_modules(TypeMods) ->
     foldl(fun({_,Mod}=TypeMod, A) ->
 		  case c:l(Mod) of
@@ -68,28 +117,14 @@ load_modules(TypeMods) ->
 		  end
 	  end, [], TypeMods).
 
-init_ui_plugins(Ms) ->
-    Def = fun(Missing) ->
-		  Msg = io_lib:format("Reinstall Wings. "
-				      "Missing plugin for ~p.",
-				      [Missing]),
-		  wings_io:message(lists:flatten(Msg)),
-		  aborted
-	  end,
-    init_ui_plugins(Ms, Def).
-
-init_ui_plugins([M|Ms], Ui0) ->
-    case catch M:init(Ui0) of
-	Ui when is_function(Ui) ->
-	    init_ui_plugins(Ms, Ui);
-	Other ->
-	    io:format("~w:init/1 bad return value: ~p\n", [M,Other])
-    end;
-init_ui_plugins([], Ui) -> put(wings_ui, Ui).
-
-call_ui(What) ->
-    Ui = get(wings_ui),
-    Ui(What).
+def_ui_plugin() ->
+    fun(Missing) ->
+	    Msg = io_lib:format("Reinstall Wings. "
+				"Missing plugin for ~p.",
+				[Missing]),
+	    wings_io:message(lists:flatten(Msg)),
+	    aborted
+    end.
 
 try_dirs() ->
     Dir0 = wings:root_dir(),
@@ -128,9 +163,6 @@ list_dir_1([N|Ns], Dir0, Dirs, Beams) ->
     end;
 list_dir_1([], Dir, Dirs, Beams) -> {Dirs,Beams}.
     
-rearrange(N, T, Type, M, {C}) -> {N,{T,{[Type,M,C]}}};
-rearrange(N, T, Type, M, C) -> {N,{T,[Type,M,C]}}.
-
 to_module([_,_,T|_]=Beam) ->
     {convert_type(T),list_to_atom(filename:rootname(Beam))}.
 
@@ -140,33 +172,19 @@ convert_type($8) -> user_interface;
 convert_type($9) -> user_interface;
 convert_type($u) -> unsupported.
 
-menu(Name, Menu) ->
-    Dict = get(wings_plugins),
-    case gb_trees:lookup(Name, Dict) of
-	none ->
-	    Menu;
-	{value,PluginMenu} ->
-	    list_to_tuple(tuple_to_list(Menu) ++ [separator|PluginMenu])
-    end.       
-
-command({[Type,Mod,Cmd]}, St) -> command(Type, Mod, Cmd, true, St);
-command([Type,Mod,Cmd], St) -> command(Type, Mod, Cmd, false, St).
-    
-command(creator, Mod, Cmd, Ask, St) ->
-    check_result(Mod:command(Cmd, Ask), St);
-command(unsupported, Mod, Cmd, Ask, St) ->
-    check_result(Mod:command(Cmd, Ask, St), St).
-
-check_result({new_shape,Prefix,#e3d_object{}=Obj,Mat}, St0) ->
+check_result(M, {new_shape,Prefix,#e3d_object{}=Obj,Mat}, St0) ->
     {We,UsedMat} = import_object(Obj),
     St = add_materials(UsedMat, Mat, St0),
     new_shape(Prefix, We, St);
-check_result({new_shape,Prefix,Fs,Vs}, St) ->
+check_result(M, {new_shape,Prefix,Fs,Vs}, St) ->
     We = wings_we:build(Fs, Vs),
     new_shape(Prefix, We, St);
-check_result(aborted, St) -> aborted;
-check_result({drag,_}=Drag, _) -> Drag;
-check_result(#st{}=St, _) -> St.
+check_result(M, aborted, St) -> aborted;
+check_result(M, {drag,_}=Drag, _) -> Drag;
+check_result(M, #st{}=St, _) -> St;
+check_result(M, Other, St) ->
+    io:format("~w:command/3: bad return value: ~P\n", [M,Other,20]),
+    next.
 
 new_shape(Prefix, We, #st{onext=Oid}=St) ->
     Name = Prefix++integer_to_list(Oid),
