533310f878a3485fc4e009616030b1abf4b84faf
Sean Hinde <seanhinde@users.sourceforge.net>
2002-03-30 15:37:56 +0000

    Added Mac OS X File dialogue code. Note: the compile command for the driver is in the source file

---
+++ plugins_src/mac_file/mac_wings_file_drv.c
@@ -0,0 +1,187 @@
+/*
+ *
+ *  mac_wings_file_drv.c --
+ *
+ *     Erlang driver for native file dialog boxes for Mac OS X.
+ *
+ *  Copyright (c) 2001 Patrik Nyblom
+ *
+ *  Modified to support OSX by Sean Hinde
+ *
+ *  See the file "license.terms" for information on usage and redistribution
+ *  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ *     $Id: mac_wings_file_drv.c,v 1.1 2002/03/30 15:37:56 seanhinde Exp $
+ */
+
+/*cc -ObjC -I ~/local/lib/erlang/usr/include -bundle -flat_namespace -undefined suppress -framework Cocoa -o ../../plugins/mac_file/mac_wings_file_drv.so mac_wings_file_drv.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "erl_driver.h"
+
+#import <Cocoa/Cocoa.h>
+
+#define PATH_MAX 1024
+
+/*
+** Interface routines
+*/
+static ErlDrvData mac_wings_file_start(ErlDrvPort port, char *buff);
+static void mac_wings_file_stop(ErlDrvData handle);
+static int mac_wings_file_control(ErlDrvData handle, unsigned int command, 
+			      char* buff, int count, 
+			      char** res, int res_size);
+
+/*
+** Internal routines
+*/
+
+/*
+** The driver struct
+*/
+ErlDrvEntry mac_wings_file_driver_entry = {
+    NULL,		   /* F_PTR init, N/A */
+    mac_wings_file_start,      /* L_PTR start, called when port is opened */
+    mac_wings_file_stop,       /* F_PTR stop, called when port is closed */
+    NULL,                  /* F_PTR output, called when erlang has sent */
+    NULL,                  /* F_PTR ready_input, called when input descriptor 
+			      ready */
+    NULL,                  /* F_PTR ready_output, called when output 
+			      descriptor ready */
+    "mac_wings_file_drv",      /* char *driver_name, the argument to open_port */
+    NULL,                  /* F_PTR finish, called when unloaded */
+    NULL,                  /* void * that is not used (BC) */
+    mac_wings_file_control,    /* F_PTR control, port_control callback */
+    NULL,                  /* F_PTR timeout, driver_set_timer callback */
+    NULL                   /* F_PTR outputv, reserved */
+};
+
+/*
+** Driver initialization routine
+*/
+DRIVER_INIT(mac_wings_file_drv)
+{
+    return &mac_wings_file_driver_entry;
+}
+
+/*
+** Driver interface routines
+*/
+
+/*
+** Open a port
+*/
+static ErlDrvData mac_wings_file_start(ErlDrvPort port, char *buff)
+{
+    return (ErlDrvData) 0;
+}
+
+
+/*
+** Close a port
+*/
+static void mac_wings_file_stop(ErlDrvData handle)
+{
+    return;
+}
+
+/*
+** Control message from erlang, syncronous operations which hang the
+** emulator. This is not a place where you normally do blocking
+** operations, but as the wings application is single threaded
+** it doesn't matter.
+*/
+static int mac_wings_file_control(ErlDrvData handle, unsigned int command, 
+			      char* buff, int count, 
+			      char** res, int res_size)
+{
+  int result;
+  char *rbuff;
+  char *defdir;
+    char *filter;
+    char *filter_desc;
+    char *title;
+    char *text;
+    char *defname;
+    switch (command) {
+    case 0: /* Yes/No/Cancel question */
+    case 4: /* Yes/No/Cancel question */
+      {
+	NSString *title = [NSString stringWithCString:buff]; /* Title of window */
+	NSString *text = [NSString stringWithCString:buff + [title length] + 1]; /* Prompt text */
+
+        switch (NSRunAlertPanel(title, text, @"Yes", @"No", @"Cancel")) {
+	case NSAlertDefaultReturn:
+	  strcpy(*res,"yes");
+	  return 3;
+	case NSAlertAlternateReturn:
+	  strcpy(*res,"no");
+	  return 2;
+	default:
+	  strcpy(*res,"aborted");
+	  return 7;
+	}
+      }
+    case 1: /* Open (or import) file */
+    case 2: /* Save (or export) file */
+      {
+          NSArray *fileTypes;
+          NSString *defdir1, *filter1, *filter_desc1, *title1, *defname1;
+          
+	defdir = buff; /* Default directory */
+	filter = defdir + strlen(defdir) + 1; /* Filter expression (.wings) */
+	filter_desc = filter + strlen(filter) + 1;      /* Desc. of filter */
+	title = filter_desc + strlen(filter_desc) + 1;  /* Title of dialog */
+	defname = title + strlen(title) + 1; /* Default name for file */
+
+        defdir1 = [NSString stringWithCString:defdir];
+        filter1 = [NSString stringWithCString:filter + 1]; // . not needed for mac
+        filter_desc1 = [NSString stringWithCString:filter_desc];
+        title1 = [NSString stringWithCString:title];
+        defname1 = [NSString stringWithCString:defname];
+	
+	rbuff=driver_alloc(PATH_MAX+1);
+	strcpy(rbuff, defname);
+	fileTypes = [NSArray arrayWithObject:filter1];
+	
+	if (command == 1) {
+	  NSOpenPanel *oPanel = [NSOpenPanel openPanel];
+	  [oPanel setAllowsMultipleSelection:NO];
+	  result = [oPanel runModalForDirectory:defdir1 file:nil types:fileTypes];
+	  if (result == NSOKButton) {
+	    NSString *aFile = [oPanel filename];
+	    [aFile getCString:rbuff];
+	    *res = rbuff;
+	    return strlen(rbuff);
+	  }
+	  driver_free(rbuff);
+	  return 0;
+	} else {
+	  NSSavePanel *sPanel = [NSSavePanel savePanel];
+	  result = [sPanel runModalForDirectory:defdir1 file:defname1];
+	  if (result == NSOKButton) {
+	    NSString *aFile = [sPanel filename];
+	    [aFile getCString:rbuff];
+	    *res = rbuff;
+	    return strlen(rbuff);
+	  }
+	  driver_free(rbuff);
+	  return 0;
+	}
+	return 0;
+      }
+    case 3: /* Message box */
+    {
+      NSString *text = [NSString stringWithCString:buff];
+      NSString *title = [NSString stringWithCString:buff + strlen(buff) + 1];
+      NSRunAlertPanel(title, text, nil, nil, nil);	
+      return 0;
+    }
+    default:
+        return -1; /* Error return, throws exception in erlang */
+    }
+}
---
+++ plugins_src/mac_file/wp8_mac_file.erl
@@ -0,0 +1,106 @@
+%%
+%%  wp8_file.erl --
+%%
+%%     Native file dialog boxes for Mac OS X.
+%%
+%%  Copyright (c) 2001 Patrik Nyblom
+%%
+%%  Changes for OSX by Sean Hinde : 2002/2/18
+%%
+%%  See the file "license.terms" for information on usage and redistribution
+%%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+%%
+%%     $Id: wp8_mac_file.erl,v 1.1 2002/03/30 15:37:56 seanhinde Exp $
+%%
+
+-module(wp8_mac_file).
+
+-export([menus/0, init/1]).
+
+%% Operations supported by driver.
+-define(OP_QUESTION, 0).
+-define(OP_READ, 1).
+-define(OP_WRITE, 2).
+-define(OP_MESSAGE, 3).
+-define(OP_SERIOUS_QUESTION, 4).
+
+menus() ->
+    [].
+init(Next) ->
+    case os:type() of
+	{unix,darwin} ->
+	    Dir = filename:dirname(code:which(?MODULE)),
+	    case erl_ddll:load_driver(Dir, "mac_wings_file_drv") of
+		ok ->
+		    case open_port({spawn,"mac_wings_file_drv"},[]) of
+			Port when is_port(Port) ->
+			    register(wp8_file_port, Port),
+			    fun(What) ->
+				    fileop(What,Next)
+			    end;
+			Other ->
+			    Next
+		    end;
+		Else ->
+		    Next
+	    end;
+	_ ->
+	    Next
+    end.
+
+fileop({file,overwrite,Prop},_Next) ->
+    yes; %Fixed in dialog instead
+
+fileop({question,Question}, _Next) ->
+    list_to_atom(erlang:port_control(wp8_file_port, ?OP_QUESTION,
+				     ["Wings 3D",0,Question,0]));
+
+fileop({serious_question,Question}, _Next) ->
+    list_to_atom(erlang:port_control(wp8_file_port, ?OP_SERIOUS_QUESTION,
+				     ["Wings 3D",0,Question,0]));
+
+fileop({message,Message}, _Next) ->
+    Title = "Wings 3D",
+    erlang:port_control(wp8_file_port, ?OP_MESSAGE, [Title,0,Message,0]);
+
+fileop({file,open,Prop}, _Next) ->
+    file_dialog(?OP_READ, Prop, "Open Wings 3D file");
+
+fileop({file,save,Prop}, _Next) ->
+    file_dialog(?OP_WRITE, Prop, "Save Wings 3D file");
+
+fileop({file,import,Prop}, _Next) ->
+    file_dialog(?OP_READ, Prop, "Import file into Wings 3D");
+
+fileop({file,export,Prop}, _Next) ->
+    file_dialog(?OP_WRITE, Prop, "Export file from Wings 3D");
+
+fileop({file,merge,Prop}, _Next) ->
+    file_dialog(?OP_READ, Prop, "Merge Wings 3D file");
+
+fileop(What, Next) ->
+%     io:format("Default called for ~p~n",[What]),
+%     Ret=Next(What),
+%     io:format("Default returned ~p~n",[Ret]),
+%     Ret.
+    Next(What).
+
+file_dialog(Type, Prop, Title) ->
+    Ext = property_lists:get_value(ext, Prop, ".wings"),
+    ExtDesc = property_lists:get_value(ext_desc, Prop, "Default type"),
+
+    Dir = case get(wp8_file_defdir) of
+	      undefined ->
+		  [];
+	      DefDir ->
+		  filename:nativename(DefDir)
+	  end,
+    DefName = property_lists:get_value(default_filename, Prop, ""),
+    Data = [Dir,0,Ext,0,ExtDesc,0,Title,0,DefName,0],
+    case erlang:port_control(wp8_file_port, Type, Data) of
+	[] ->
+	    aborted;
+	Else ->
+	    put(wp8_file_defdir,filename:dirname(Else)),
+	    filename:absname(Else) % Happens to turn windows slashes...
+    end.
