0eff414a519243d94b45505cb7a53178cc41fbe5
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-05-08 10:04:15 +0000

    Removed CRs.

--- src/wings_vertex.erl
+++ src/wings_vertex.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_vertex.erl,v 1.21 2002/05/08 10:00:48 bjorng Exp $
+%%     $Id: wings_vertex.erl,v 1.22 2002/05/08 10:04:15 bjorng Exp $
 %%
 
 -module(wings_vertex).
