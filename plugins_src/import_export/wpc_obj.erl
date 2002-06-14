b584a82c11886d3024246cf7785ba7b5b6bc477c
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-06-14 13:08:17 +0000

    Added a user-definable scale factor to OBJ import/export.

--- plugins_src/import_export/wpc_obj.erl
+++ plugins_src/import_export/wpc_obj.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wpc_obj.erl,v 1.1 2002/01/28 17:33:40 bjorng Exp $
+%%     $Id: wpc_obj.erl,v 1.2 2002/06/14 13:08:17 bjorng Exp $
 %%
 
 -module(wpc_obj).
@@ -26,36 +26,91 @@ menu({file,export_selected}, Menu) ->
     menu_entry(Menu);
 menu(_, Menu) -> Menu.
 
-command({file,{import,obj}}, St) ->
-    Props = props(),
-    wpa:import(Props, fun import/1, St);
-command({file,{export,obj}}, St) ->
-    Props = props(),
-    wpa:export(Props, fun export/2, St);
-command({file,{export_selected,obj}}, St) ->
-    Props = props(),
-    wpa:export_selected(Props, fun export/2, St);
-command(Cmd, _) ->
+command({file,{import,{obj,Ask}}}, St) ->
+    do_import(Ask, St);
+command({file,{export,{obj,Ask}}}, St) ->
+    Exporter = fun(Ps, Fun) -> wpa:export(Ps, Fun, St) end,
+    do_export(Ask, Exporter, St);
+command({file,{export_selected,{obj,Ask}}}, St) ->
+    Exporter = fun(Ps, Fun) -> wpa:export_selected(Ps, Fun, St) end,
+    do_export(Ask, Exporter, St);
+command(_, _) ->
     next.
 
 menu_entry(Menu) ->
-    Menu ++ [{"Wavefront (.obj)",obj}].
+    Menu ++ [{"Wavefront (.obj)",obj,[option]}].
 
 props() ->
     [{ext,".obj"},{ext_desc,"Wavefront File"}].
 
-import(Filename) ->
-    case e3d_obj:import(Filename) of
-	{ok,E3dFile} ->
-	    {ok,E3dFile};
-	{error,Error} ->
-	    {error,Error}
+%%%
+%%% Import.
+%%%
+
+do_import(Ask, St) when is_atom(Ask) ->
+    wpa:dialog(Ask, dialog(import), St,
+	       fun(Res) ->
+		       {file,{import,{obj,Res}}}
+	       end);
+do_import(Attr, St) ->
+    set_pref(Attr),
+    wpa:import(props(), import_fun(Attr), St).
+
+import_fun(Attr) ->
+    fun(Filename) ->
+	    case e3d_obj:import(Filename) of
+		{ok,E3dFile0} ->
+		    E3dFile = import_transform(E3dFile0, Attr),
+		    {ok,E3dFile};
+		{error,Error} ->
+		    {error,Error}
+	    end
     end.
 
-export(Filename, Contents) ->
-    case e3d_obj:export(Filename, Contents) of
-	ok ->
-	    ok;
-	{error,Error} ->
-	    {error,Error}
+%%%
+%%% Export.
+%%%
+
+do_export(Ask, _Exporter, St) when is_atom(Ask) ->
+    wpa:dialog(Ask, dialog(export), St,
+	       fun(Res) ->
+		       {file,{export,{obj,Res}}}
+	       end);
+do_export(Attr, Exporter, _St) when is_list(Attr) ->
+    set_pref(Attr),
+    Exporter(props(), export_fun(Attr)).
+
+export_fun(Attr) ->
+    fun(Filename, Contents) ->
+	    export_1(Filename, Contents, Attr)
     end.
+
+export_1(Filename, Contents0, Attr) ->
+    Contents = export_transform(Contents0, Attr),
+    case e3d_obj:export(Filename, Contents, Attr) of
+	ok -> ok;
+	{error,_}=Error -> Error
+    end.
+
+dialog(import) ->
+    common_dialog();
+dialog(export) ->
+    [{"One group per material",get_pref(group_per_material, true),
+      [{key,group_per_material}]}|common_dialog()].
+
+common_dialog()->
+    [{label,"Scale"},{text,get_pref(scale, 1.0),[{key,scale}]}].
+
+get_pref(Key, Def) ->
+    wpa:pref_get(?MODULE, Key, Def).
+
+set_pref(KeyVals) ->
+    wpa:pref_set(?MODULE, KeyVals).
+
+export_transform(Contents, Attr) ->
+    Mat = e3d_mat:scale(property_lists:get_value(scale, Attr, 1.0)),
+    e3d_file:transform(Contents, Mat).
+
+import_transform(Contents, Attr) ->
+    Mat = e3d_mat:scale(1.0/property_lists:get_value(scale, Attr, 1.0)),
+    e3d_file:transform(Contents, Mat).
