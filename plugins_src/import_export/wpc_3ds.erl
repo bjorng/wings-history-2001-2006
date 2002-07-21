2d98209ef838749b80c5de366c91e0cbe68b99dd
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-07-21 15:49:12 +0000

    Added a dialog box with scale options for 3DS import/export.

--- plugins_src/import_export/wpc_3ds.erl
+++ plugins_src/import_export/wpc_3ds.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wpc_3ds.erl,v 1.1 2002/01/28 17:33:40 bjorng Exp $
+%%     $Id: wpc_3ds.erl,v 1.2 2002/07/21 15:49:12 bjorng Exp $
 %%
 
 -module(wpc_3ds).
@@ -26,37 +26,89 @@ menu({file,export_selected}, Menu) ->
     menu_entry(Menu);
 menu(_, Menu) -> Menu.
 
-command({file,{import,tds}}, St) ->
-    Props = props(),
-    wpa:import(Props, fun import/1, St);
-command({file,{export,tds}}, St) ->
-    Props = props(),
-    wpa:export(Props, fun export/2, St);
-command({file,{export_selected,tds}}, St) ->
-    Props = props(),
-    wpa:export_selected(Props, fun export/2, St);
-command(Cmd, _) ->
+command({file,{import,{tds,Ask}}}, St) ->
+    do_import(Ask, St);
+command({file,{export,{tds,Ask}}}, St) ->
+    Exporter = fun(Ps, Fun) -> wpa:export(Ps, Fun, St) end,
+    do_export(Ask, Exporter, St);
+command({file,{export_selected,{tds,Ask}}}, St) ->
+    Exporter = fun(Ps, Fun) -> wpa:export_selected(Ps, Fun, St) end,
+    do_export(Ask, Exporter, St);
+command(_, _) ->
     next.
 
 menu_entry(Menu) ->
-    Menu ++ [{"3D Studio (.3ds)",tds}].
+    Menu ++ [{"3D Studio (.3ds)",tds,[option]}].
 
 props() ->
     [{ext,".3ds"},{ext_desc,"3D Studio File"}].
 
-import(Filename) ->
-    case e3d_tds:import(Filename) of
-	{ok,E3dFile} ->
-	    {ok,E3dFile};
-	{error,Error} ->
-	    {error,Error}
+%%%
+%%% Import.
+%%%
+
+do_import(Ask, St) when is_atom(Ask) ->
+    wpa:dialog(Ask, dialog(import), St,
+	       fun(Res) ->
+		       {file,{import,{tds,Res}}}
+	       end);
+do_import(Attr, St) ->
+    set_pref(Attr),
+    wpa:import(props(), import_fun(Attr), St).
+
+import_fun(Attr) ->
+    fun(Filename) ->
+	    case e3d_tds:import(Filename) of
+		{ok,E3dFile0} ->
+		    E3dFile = import_transform(E3dFile0, Attr),
+		    {ok,E3dFile};
+		{error,Error} ->
+		    {error,Error}
+	    end
     end.
 
-export(Filename, Contents) ->
-    case e3d_tds:export(Filename, Contents) of
-	ok ->
-	    ok;
-	{error,Error} ->
-	    {error,Error}
+%%%
+%%% Export.
+%%%
+
+do_export(Ask, _Exporter, St) when is_atom(Ask) ->
+    wpa:dialog(Ask, dialog(export), St,
+	       fun(Res) ->
+		       {file,{export,{tds,Res}}}
+	       end);
+do_export(Attr, Exporter, _St) when is_list(Attr) ->
+    set_pref(Attr),
+    Exporter(props(), export_fun(Attr)).
+
+export_fun(Attr) ->
+    fun(Filename, Contents) ->
+	    export_1(Filename, Contents, Attr)
     end.
 
+export_1(Filename, Contents0, Attr) ->
+    Contents = export_transform(Contents0, Attr),
+    case e3d_tds:export(Filename, Contents, Attr) of
+	ok -> ok;
+	{error,_}=Error -> Error
+    end.
+
+dialog(import) ->
+    [{label,"Import scale"},{text,get_pref(import_scale, 1.0),[{key,import_scale}]},
+     {label,"(Export scale)"},{text,get_pref(export_scale, 1.0),[{key,export_scale}]}];
+dialog(export) ->
+    [{label,"(Import scale)"},{text,get_pref(import_scale, 1.0),[{key,import_scale}]},
+     {label,"Export scale"},{text,get_pref(export_scale, 1.0),[{key,export_scale}]}].
+
+get_pref(Key, Def) ->
+    wpa:pref_get(?MODULE, Key, Def).
+
+set_pref(KeyVals) ->
+    wpa:pref_set(?MODULE, KeyVals).
+
+export_transform(Contents, Attr) ->
+    Mat = e3d_mat:scale(property_lists:get_value(export_scale, Attr, 1.0)),
+    e3d_file:transform(Contents, Mat).
+
+import_transform(Contents, Attr) ->
+    Mat = e3d_mat:scale(property_lists:get_value(import_scale, Attr, 1.0)),
+    e3d_file:transform(Contents, Mat).
