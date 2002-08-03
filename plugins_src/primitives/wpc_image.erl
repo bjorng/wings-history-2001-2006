e13ad4c098245736e6d0694cd25ec9e46a7db6aa
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2002-08-03 08:30:22 +0000

    The Image primitive will now pad the source image dimensions to a power of two.

--- plugins_src/primitives/wpc_image.erl
+++ plugins_src/primitives/wpc_image.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wpc_image.erl,v 1.2 2002/08/03 06:56:34 bjorng Exp $
+%%     $Id: wpc_image.erl,v 1.3 2002/08/03 08:30:22 bjorng Exp $
 %%
 
 -module(wpc_image).
@@ -17,6 +17,8 @@
 -include_lib("gl.hrl").
 -include_lib("e3d.hrl").
 -include_lib("e3d_image.hrl").
+
+-import(lists, [reverse/1]).
 
 init() ->
     true.
@@ -56,17 +58,21 @@ make_image(Format) ->
 	    end
     end.
 
-make_image_1({W,H,Pixels}=Image) ->
+make_image_1({W0,H0,_}=Image0) ->
+    {W,H,Pixels} = Image = pad_image(Image0),
+    io:format("~p\n", [{W,H}]),
     case can_texture_be_loaded(Image) of
 	false ->
 	    wings_util:error("The image cannot be loaded as a texture "
 			     "(it is probably too large or "
 			     " has wrong dimensions).");
 	true ->
+	    MaxU = W0/W,
+	    MaxV = H0/H,
 	    M = [image],
 	    Fs = [#e3d_face{vs=[0,3,2,1],tx=[1,0,3,2],mat=M},
 		  #e3d_face{vs=[1,2,3,0],tx=[2,3,0,1],mat=M}],
-	    Tx = [{0.0,0.0},{1.0,0.0},{1.0,1.0},{0.0,1.0}],
+	    Tx = [{0.0,0.0},{MaxU,0.0},{MaxU,MaxV},{0.0,MaxV}],
 	    {X,Y} = ratio(W, H),
 	    Vs = [{0.0,-Y,-X},{0.0,Y,-X},{0.0,Y,X},{0.0,-Y,X}],
 	    Mesh = #e3d_mesh{type=polygon,fs=Fs,vs=Vs,tx=Tx},
@@ -90,4 +96,48 @@ props(bmp) -> [{ext,".bmp"},{ext_desc,"BMP Bitmap"}];
 props(tif) -> [{ext,".tif"},{ext_desc,"Tiff Bitmap"}];
 props(tga) -> [{ext,".tga"},{ext_desc,"Targa File"}].
 
-    
+pad_image({W0,H,Pixels0}=Image) ->
+    case nearest_power_two(W0) of
+	W0 ->
+	    pad_image_1(Image);
+	W ->
+	    Pad = zeroes(3*(W-W0)),
+	    Pixels = pad_rows(Pixels0, 3*W0, Pad, []),
+	    pad_image_1({W,H,Pixels})
+    end.
+
+pad_image_1({W,H0,Pixels0}=Image) ->
+    case nearest_power_two(H0) of
+	H0 ->
+	    pad_image_2(Image);
+	H ->
+	    Pad = zeroes(3*W*(H-H0)),
+	    Pixels = [Pixels0|Pad],
+	    pad_image_2({W,H,Pixels})
+    end.
+
+pad_image_2({W,H,Pixels}=Image) when is_list(Pixels) ->
+    {W,H,list_to_binary(Pixels)};
+pad_image_2(Image) -> Image.
+
+pad_rows(Bin, W, Pad, Acc) ->
+    case Bin of
+	<<>> -> reverse(Acc);
+	<<Row:W/binary,T/binary>> ->
+	    pad_rows(T, W, Pad, [[Row|Pad]|Acc])
+    end.
+
+zeroes(0) -> [];
+zeroes(1) -> [0];
+zeroes(N) when N rem 2 == 0 ->
+    Z = zeroes(N div 2),
+    [Z|Z];
+zeroes(N) ->
+    Z = zeroes(N div 2),
+    [0,Z|Z].
+
+nearest_power_two(N) ->
+    nearest_power_two(N, 1).
+
+nearest_power_two(N, B) when N =< B -> B;
+nearest_power_two(N, B) -> nearest_power_two(N, B bsl 1).
