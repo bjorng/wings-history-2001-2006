613275c985b753bd72c22a73a225691794677f11
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2003-01-21 20:15:25 +0000

    Added two new fields to #e3d_image{}.

--- e3d/e3d_image.hrl
+++ e3d/e3d_image.hrl
@@ -8,17 +8,20 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: e3d_image.hrl,v 1.3 2002/11/26 09:46:26 dgud Exp $
+%%     $Id: e3d_image.hrl,v 1.4 2003/01/21 20:15:25 bjorng Exp $
 %%
 
--record(e3d_image,           %% Current supported formats
+-record(e3d_image,           %% Currently supported formats:
 	{type = r8g8b8,      %% [g8 (gray8) or a8 (alpha8) 
 	                     %% r8g8b8 or r8g8b8a8 or b8g8r8 or b8g8r8a8]
 	 bytes_pp = 3,       %% bytes per pixel
 	 alignment = 1,      %% A = 1|2|4 Next row starts direct|even 2|even 4
-	 order = lower_left, %% First pixel is in [lower_left, lower_right, upper_left, upper_right]
+	 order = lower_left, %% First pixel is in:
+	                     %% lower_left,lower_right,upper_left,upper_right]
 	 width = 0,          %% in pixels
 	 height = 0,         %% in pixels
-	 image               %% binary
+	 image,              %% binary
+	 filename=none,	     %% Filename or none
+	 name=[]	     %% Name of image
 	}).
 
