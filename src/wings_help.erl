8f22bd409d39d87a17d8beecbad6e3e2c5cea379
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2003-03-09 09:45:45 +0000

    Help texts are shown in proper windows.

--- src/wings_help.erl
+++ src/wings_help.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_help.erl,v 1.42 2003/01/27 18:09:50 bjorng Exp $
+%%     $Id: wings_help.erl,v 1.43 2003/03/09 09:45:45 bjorng Exp $
 %%
 
 -module(wings_help).
@@ -31,7 +31,6 @@ menu(_) ->
      {"Default Commands",default_commands},
      separator,
      {"OpenGL Info",opengl_info},
-     {"OpenGL Extensions",opengl_extensions},
      separator,
      {"About Wings 3D",about}].
 
@@ -51,14 +50,11 @@ command(advanced_menus, _St) ->
     advanced_menus();
 command(opengl_info, _St) ->
     opengl_info();
-command(opengl_extensions, _St) ->
-    opengl_extensions();
 command(about, _St) ->
     about().
 
 getting_started() ->
-    Help = ["Getting Started",
-	    "When learning Wings, keep an eye at the status line at "
+    Help = ["When learning Wings, keep an eye at the status line at "
 	    "bottom of the screen. Generally it shows what the "
 	    "mouse buttons will do at any given moment.",
 
@@ -75,29 +71,26 @@ getting_started() ->
 	    "and the [R] (right mouse button) to access the context-sensitive "
 	    "pop-up menus."
 	   ],
-    help_window(Help).
+    help_window("Getting Started", Help).
 
 one_or_two() ->
-    Help = ["Using a mouse with One or Two buttons",
-	    "To use mice with only one or two buttons, "
+    Help = ["To use mice with only one or two buttons, "
 	    "you must inform Wings how many buttons your mouse has "
 	    "in the Edit|Camera Mode dialog box."],
-    help_window(Help).
+    help_window("Using a mouse with One or Two buttons", Help).
 
 advanced_menus() ->
-    Help = ["Advanced Menus",
-	    "In the Edit|Advanced Preferences dialog box, there is a check box "
+    Help = ["In the Edit|Advanced Preferences dialog box, there is a check box "
 	    "for \"Advanced Menus\".",
 
 	    "With Advanced Menus turned on, many menu commands do "
 	    "different things depending on which mouse-button you invoke "
 	    "them with. For instance, [R] clicking on the Move command "
 	    "allows you to specify a direction vector to move along."],
-    help_window(Help).
+    help_window("Advanced Menus", Help).
 
 international() ->
-    Help = ["French And German Keyboards",
-	    "Unfortunately, on French and German keyboards (and possibly "
+    Help = ["Unfortunately, on French and German keyboards (and possibly "
 	    "others), the Undo/Redo commands will not be bound to the [Z] "
 	    "key. (That might be changed in a future release of Wings.)",
 
@@ -106,11 +99,10 @@ international() ->
 
 	    "On German keyboards, the Undo/Redo commands are found on the "
 	    "[Y] key ([Ctrl]+[Y], [Ctrl]+[Alt]+[Y] and so on)."],
-    help_window(Help).
+    help_window("French And German Keyboards", Help).
 
 def_commands() ->
-    Help = ["Assigning Default Commands",
-	    "In the Edit|Advanced Preferences dialog box, you can turn on "
+    Help = ["In the Edit|Advanced Preferences dialog box, you can turn on "
 	    "\"Default Commands\".",
 
 	    "Two default commands can be defined. To save the "
@@ -123,11 +115,10 @@ def_commands() ->
 	    "  [Ctrl]+[M]",
 	    "Note: When using the 3ds max or Blender camera modes, the second "
 	    "default command cannot be used."],
-    help_window(Help).
+    help_window("Assigning Default Commands", Help).
 
 def_hotkeys() ->
-    Help = ["Assigning Hotkeys",
-	    "Any command that appears in a menu, can be assigned a "
+    Help = ["Any command that appears in a menu, can be assigned a "
 	    "keyboard short-cut (hotkey).",
 	    "To assign a hotkey to a command, open the menu containing "
 	    "the command. "
@@ -135,24 +126,22 @@ def_hotkeys() ->
 	    "and then press the key you want to assign the command to.",
 	    "To delete a hotkey, similarly high-light the command in a "
 	    "menu, and press the [Del] or [\\] key."],
-    help_window(Help).
+    help_window("Assigning Hotkeys", Help).
 
 lights() ->
-    Help = ["Light Basics",
-	    "1. Create lights using the Light command in the primitives "
+    Help = ["1. Create lights using the Light command in the primitives "
 	    "menu ([R]-click when there is no selection).",
 	    "2. Select a light by [L]-clicking on it. When any light is "
 	    "selected, a special Light menu will pop up when you [R]-click.",
 	    "3. To tell Wings to actually use the lights you have created, "
 	    "use the View|Scene Lights command."],
-    help_window(Help).
+    help_window("Light Basics", Help).
 
 opengl_info() ->
-    Help = ["Basic OpenGL Info",
+    Help = [
 	    "Vendor: " ++ gl:getString(?GL_VENDOR) ++ "\n" ++
 	    "Renderer: " ++ gl:getString(?GL_RENDERER) ++ "\n" ++
 	    "Version: " ++ gl:getString(?GL_VERSION),
-	    "Detailed Info",
 	    deep_info([{"Red size",?SDL_GL_RED_SIZE},
 		       {"Green size",?SDL_GL_GREEN_SIZE},
 		       {"Blue size",?SDL_GL_BLUE_SIZE},
@@ -163,13 +152,11 @@ opengl_info() ->
 		       {"Accum. red size",?SDL_GL_ACCUM_RED_SIZE},
 		       {"Accum. green size",?SDL_GL_ACCUM_GREEN_SIZE},
 		       {"Accum. blue size",?SDL_GL_ACCUM_BLUE_SIZE},
-		       {"Accum. alpha size",?SDL_GL_ACCUM_ALPHA_SIZE}])],
-    help_window(Help).
-
-opengl_extensions() ->
-    Help = ["OpenGL Extensions",
-	    gl:getString(?GL_EXTENSIONS)],
-    help_window(Help).
+		       {"Accum. alpha size",?SDL_GL_ACCUM_ALPHA_SIZE}]),
+%%	    "# compressed texture formats: " ++
+%%	    integer_to_list(hd(gl:getIntegerv(?GL_NUM_COMPRESSED_TEXTURE_FORMATS))),
+	    "OpenGL Extensions",gl:getString(?GL_EXTENSIONS)],
+    help_window("OpenGL Info", Help).
 
 deep_info([{Label,Attr}|T]) ->
     Label ++ ": " ++ integer_to_list(sdl_video:gl_getAttribute(Attr)) ++ "\n" ++
@@ -177,43 +164,59 @@ deep_info([{Label,Attr}|T]) ->
 deep_info([]) -> [].
 
 about() ->
-    Op = {push,fun(Ev) ->
-		       handle_help_event(Ev, splash)
-	       end},
     Xs = 280,
     Ys = 176,
     {W,H} = wings_wm:top_size(),
     X = trunc((W-Xs) / 2),
     Y = trunc((H-Ys) / 2),
+    Op = {push,fun handle_splash_event/1},
     wings_wm:new(help, {X,Y,highest}, {Xs,Ys+?LINE_HEIGHT}, Op),
     wings_wm:grab_focus(help),
     wings_wm:dirty(),
     keep.
 
-help_window(Text) ->
-    create_help_window(Text, 0, []).
+%%%
+%%% Scrollable help window.
+%%%
+-record(ts,
+	{lines,
+	 first,
+	 tw,
+	 th
+	 }).
+	 
 
-create_help_window([S|T], Rows, Acc) ->
-    break_line(S, T, Rows, Acc);
-create_help_window([], Rows, Lines) ->
-    Xs = 64*?CHAR_WIDTH,
-    Ys = Rows*?LINE_HEIGHT,
+help_window(Title, Text) ->
+    help_window(help, Title, Text).
+
+help_window(Name, Title, Text) ->
+    wings_wm:delete(Name),
+    {Rows,Lines} = collect_lines(Text, 0, []),
     {W,H} = wings_wm:top_size(),
+    MaxH = trunc(H*0.75),
+    Xs = 64*?CHAR_WIDTH,
+    Ys = case Rows*?LINE_HEIGHT of
+	     Ys0 when Ys0 > MaxH -> MaxH;
+	     Ys0 -> Ys0
+	 end,
     X = trunc((W-Xs) / 2),
     Y = trunc((H-Ys) / 2),
-    DrawData = {reverse(Lines),Xs,Ys},
-    Op = {push,fun(Ev) ->
-		       handle_help_event(Ev, DrawData)
-	       end},
-    wings_wm:new(help, {X,Y,highest}, {Xs+?CHAR_WIDTH,Ys+?LINE_HEIGHT}, Op),
-    wings_wm:grab_focus(help),
-    wings_wm:dirty(),
-    keep.
+    Ts = #ts{lines=reverse(Lines),first=0,tw=Xs,th=Ys0},
+    Op = {seq,push,get_help_event(Ts)},
+    Size = {Xs+?CHAR_WIDTH,Ys+?LINE_HEIGHT},
+    wings_wm:toplevel(Name, Title, {X,Y,highest}, Size,
+		      [closable,vscroller], Op),
+    wings_wm:dirty().
+
+collect_lines([S|T], Rows, Acc) ->
+    break_line(S, T, Rows, Acc);
+collect_lines([], Rows, Lines) ->
+    {Rows,Lines}.
 
 break_line(S, T, Rows, Acc) ->
     case break_line_1(S) of
 	done ->
-	    create_help_window(T, Rows+1, [[]|Acc]);
+	    collect_lines(T, Rows+1, [[]|Acc]);
 	{Line,More} ->
 	    break_line(More, T, Rows+1, [Line|Acc])
     end.
@@ -237,33 +240,59 @@ skip_blanks([$\n|T]) -> skip_blanks(T);
 skip_blanks([$\s|T]) -> skip_blanks(T);
 skip_blanks(T) -> T.
 
+get_help_event(Ts) ->
+    {replace,fun(Ev) ->
+		     handle_help_event(Ev, Ts)
+	     end}.
+
 handle_help_event(redraw, DrawData) ->
     wings_wm:message("[L] Close help window"),
     redraw(DrawData),
     keep;
-handle_help_event(#mousemotion{}, _) -> keep;
-handle_help_event(got_focus, _) -> keep;
-handle_help_event(lost_focus, _) -> keep;
-handle_help_event(quit=Ev, _) ->
-    wings_io:putback_event(Ev),
-    delete;
-handle_help_event(_, _) -> delete.
+handle_help_event(close, _) -> delete;
+handle_help_event({set_knob_pos,Pos}, #ts{th=Th}=Ts0) ->
+    Ts = Ts0#ts{first=trunc(Th*Pos) div ?LINE_HEIGHT},
+    update_scroller(Ts),
+    get_help_event(Ts);
+handle_help_event(scroll_page_up, Ts) ->
+    zoom_step(-10, Ts);
+handle_help_event(scroll_page_down, Ts) ->
+    zoom_step(10, Ts);
+handle_help_event(_, _) -> keep.
 
-redraw({Lines,Xs,Ys}) ->
+zoom_step(Step, #ts{first=First0}=Ts0) ->
+    First = case First0+Step of
+		Neg when Neg < 0 -> 0;
+		First1 -> First1
+	    end,
+    Ts = Ts0#ts{first=First},
+    update_scroller(Ts),
+    get_help_event(Ts).
+
+redraw(#ts{lines=Lines0,first=First}=Ts) ->
+    update_scroller(Ts),
     wings_io:ortho_setup(),
-    wings_io:raised_rect(0, 0, Xs, Ys),
-    gl:color3f(0.0, 0.0, 0.0),
-    gl:recti(3, 3, Xs-3, Ys-3),
-    gl:color3f(1.0, 1.0, 1.0),
-    gl:recti(4, 4, Xs-4, Ys-4),
-    gl:color3f(1.0, 0.0, 1.0),
+    {W,H} = wings_wm:win_size(),
+    wings_io:border(0, 0, W-1, H-1, {1.0,1.0,1.0}),
     gl:color3f(0.0, 0.0, 0.0),
     gl:translated(4, 4+?LINE_HEIGHT, 0),
+    Lines = lists:nthtail(First, Lines0),
     foldl(fun(L, Y) ->
 		  wings_io:text_at(5, Y, L),
 		  Y+?LINE_HEIGHT
-	  end, 0, Lines);
-redraw(splash) ->
+	  end, 0, Lines).
+
+update_scroller(#ts{first=First,th=Th}) ->
+    {_,H} = wings_wm:win_size(),
+    Name = wings_wm:active_window(),
+    wings_wm:set_knob(Name, First*?LINE_HEIGHT/Th, H/Th).
+
+%%%
+%%% Help|About (splash screen).
+%%%
+
+handle_splash_event(redraw) ->
+    wings_wm:message("[L] Close help window"),
     wings_io:ortho_setup(),
     {Xs,Ys} = wings_wm:win_size(),
     wings_io:raised_rect(0, 0, Xs, Ys),
@@ -278,4 +307,9 @@ redraw(splash) ->
     gl:disable(?GL_TEXTURE_2D),
     gl:color3f(0.0, 0.0, 0.0),
     wings_io:text_at(10, 155, "Wings 3D " ++ ?WINGS_VERSION),
-    wings_io:text_at(10, 180, "http://www.wings3d.com").
+    wings_io:text_at(10, 180, "http://www.wings3d.com"),
+    keep;
+handle_splash_event(#mousemotion{}) -> keep;
+handle_splash_event(got_focus) -> keep;
+handle_splash_event(lost_focus) -> keep;
+handle_splash_event(_) -> delete.
