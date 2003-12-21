ec1a1076b24e292fc9a12a5023f405f00d93f6cd
Bjorn Gustavsson <bjorng@users.sourceforge.net>
2003-12-21 19:12:02 +0000

    First draft of new file dialogs.

--- plugins_src/wp9_dialogs.erl
+++ plugins_src/wp9_dialogs.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wp9_dialogs.erl,v 1.20 2003/11/29 07:14:05 bjorng Exp $
+%%     $Id: wp9_dialogs.erl,v 1.21 2003/12/21 19:12:02 bjorng Exp $
 %%
 
 -module(wp9_dialogs).
@@ -21,6 +21,9 @@ init(Next) ->
 ui({file,open_dialog,Prop}, _Next) ->
     Title = proplists:get_value(title, Prop, "Open"),
     open_file(Title ++ ": ", Prop);
+ui({file,open_dialog,Prop,Cont}, _Next) ->
+    Title = proplists:get_value(title, Prop, "Open"),
+    open_dialog(Title, Prop, Cont);
 ui({file,save_dialog,Prop}, _Next) ->
     Title = proplists:get_value(title, Prop, "Save"),
     save_file(Title ++ ": ", Prop);
@@ -31,14 +34,14 @@ ui({image,read,Prop}, _Next) ->
 ui(What, Next) -> Next(What).
 
 open_file(Prompt, Prop) ->
-    Exts = file_filters(Prop),
+    Exts = file_filters_old(Prop),
     case wings_getline:filename(Prompt, Exts) of
 	aborted -> aborted;
 	Name -> ensure_extension(Name, Exts)
     end.
 
 save_file(Prompt, Prop) ->
-    Exts = file_filters(Prop),
+    Exts = file_filters_old(Prop),
     case wings_getline:filename(Prompt, Exts) of
 	aborted -> aborted;
 	Name0 ->
@@ -55,18 +58,18 @@ save_file(Prompt, Prop) ->
 	    end
     end.
 
-file_filters(Prop) ->
+file_filters_old(Prop) ->
     case proplists:get_value(extensions, Prop, none) of
 	none ->
 	    Ext = proplists:get_value(ext, Prop, ".wings"),
 	    [Ext];
 	Exts ->
-	    file_filters_1(Exts, [])
+	    file_filters_old_1(Exts, [])
     end.
 
-file_filters_1([{Ext,_Desc}|T], Acc) ->
-    file_filters_1(T, [Ext|Acc]);
-file_filters_1([], Acc) -> reverse(Acc).
+file_filters_old_1([{Ext,_Desc}|T], Acc) ->
+    file_filters_old_1(T, [Ext|Acc]);
+file_filters_old_1([], Acc) -> reverse(Acc).
 
 ensure_extension(Name, [Ext]) ->
     case eq_extensions(Ext, filename:extension(Name)) of
@@ -103,3 +106,38 @@ image_formats(Fs0) ->
     Fs3 = sofs:relation_to_family(Fs2),
     Fs = sofs:to_external(Fs3),
     [{Ext,Desc} || {Ext,[Desc|_]} <- Fs].
+
+
+%%%
+%%% File dialogs implemented using the dialog handler.
+%%%
+
+open_dialog(Title, Props, Cont) ->
+    [{_,Def}|_] = Exts = file_filters(Props),
+    Qs = {vframe,
+	  [{hframe,
+	    [{vframe,
+	      [{label,"File name"},
+	       {label,"File format"}]},
+	     {vframe,
+	      [{text,""},
+	       {menu,Exts,Def}]},
+	     {vframe,[{button,Title,fun([Name|_]) -> Cont(Name) end,[ok]},
+		      {button,"Cancel",fun(_) -> Cont(aborted) end,[cancel]}]}]}]},
+    wings_ask:dialog(Title, Qs, fun(_) -> ignore end).
+
+file_filters(Prop) ->
+    Exts0 = case proplists:get_value(extensions, Prop, none) of
+		none ->
+		    Ext = proplists:get_value(ext, Prop, ".wings"),
+		    ExtDesc = proplists:get_value(ext_desc, Prop,  "Wings File"),
+		    [{Ext,ExtDesc}];
+		Other -> Other
+	    end,
+    Exts = Exts0 ++ [{".*","All Files"}],
+    [file_filter(Es) || Es <- Exts].
+
+file_filter({"."++Ext0,Desc0}) ->
+    Ext = list_to_atom(Ext0),
+    Desc = Desc0 ++ " (*." ++ Ext0 ++ ")",
+    {Desc,Ext}.
