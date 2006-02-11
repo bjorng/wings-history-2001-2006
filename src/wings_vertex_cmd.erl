5dd5e6b6d77f22112c3238a8e43ffd4c0c35c066
Andrzej Giniewicz <giniu@users.sourceforge.net>
2006-02-11 20:20:01 +0000

    added second weld mode - move

--- src/wings_vertex_cmd.erl
+++ src/wings_vertex_cmd.erl
@@ -8,7 +8,7 @@
 %%  See the file "license.terms" for information on usage and redistribution
 %%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 %%
-%%     $Id: wings_vertex_cmd.erl,v 1.63 2006/02/07 12:42:50 giniu Exp $
+%%     $Id: wings_vertex_cmd.erl,v 1.64 2006/02/11 20:20:01 giniu Exp $
 %%
 
 -module(wings_vertex_cmd).
@@ -42,14 +42,23 @@ menu(X, Y, St) ->
 	     ?STR(menu,11,"Delete selected vertices (clearing selection)")},
 	    {?STR(menu,12,"Collapse"),collapse,
 	     ?STR(menu,13,"Delete selected vertices (creating a face selection)")},
-	    {?STR(menu,17,"Weld"),weld,
-	     ?STR(menu,18,"Weld selected vertex to another (sharing common edge)")},
+	    {?STR(menu,17,"Weld"),weld_fun(),
+	     {?STR(menu,18,"Weld selected vertex to another (sharing common edge)"),[],
+	      ?STR(menu,19,"Move selected vertices to another")},[]},
 	    separator,
 	    {?STR(menu,14,"Deform"),wings_deform:sub_menu(St)},
 	    separator,
 	    {?STR(menu,15,"Vertex Color"),vertex_color,
 	     ?STR(menu,16,"Apply vertex colors to selected vertices")}],
     wings_menu:popup_menu(X, Y, vertex, Menu).
+
+weld_fun() ->
+    fun(1, _Ns) ->
+	    {vertex,weld};
+       (3, _Ns) ->
+	    {vertex,sweld};
+       (_, _) -> ignore
+    end.
 
 %% Vertex menu.
 command({flatten,Plane}, St) ->
@@ -82,10 +91,12 @@ command(vertex_color, St) ->
     wings_color:choose(fun(Color) ->
 			       set_color(Color, St)
 		       end);
-command({'ASK',Ask}, St) ->
-    wings:ask(Ask, St, fun command/2);
 command(weld, St) ->
-    weld(St).
+    weld(St);
+command(sweld, St) ->
+    simple_weld(St);
+command({'ASK',Ask}, St) ->
+    wings:ask(Ask, St, fun command/2).
     
 %%%
 %%% The Flatten command.
@@ -713,3 +724,77 @@ transformAB(Value,[]) -> Value;
 transformAB(_Value,[{_Value,To}|_]) -> To;
 transformAB(Value,[{_,_}|Rest]) ->
    transformAB(Value,Rest).
+
+%%
+%% Simple Weld
+%%
+
+simple_weld(#st{sel=Sel,shapes=Shs}=St) ->
+   case check_mirror(Sel,Shs) of
+            no ->
+               wings_u:error(?__(1,"You cannot move vertices from mirror plane")),
+               St;
+            _ -> ok
+   end,
+   wings:ask(simple_select(), St, fun simple_weld/2).
+
+check_mirror([],_) ->
+   ok;
+check_mirror([{Obj,VSet}|Rest],Shs) ->
+   We = gb_trees:get(Obj, Shs),
+   Mirror = We#we.mirror,
+   case Mirror of
+      none -> 
+         check_mirror(Rest,Shs);
+      _ ->
+         MirrorVerts = wings_face:vertices_cw(Mirror,We),
+         MVSet = gb_sets:from_list(MirrorVerts),
+         case gb_sets:intersection(MVSet,VSet) of
+            {0,nil} -> check_mirror(Rest,Shs);
+            _ -> no
+         end
+   end.
+
+simple_select() ->
+    Desc = ?__(1,"Select target vertex for move operation"),
+    Fun = fun(check, St) -> simple_check_selection(St);
+	     (exit, {_,_,#st{sel=Vert2}=St}) ->
+		  case simple_check_selection(St) of
+		      {_,[]} -> {[],[Vert2]};
+		      {_,_} -> error
+		  end
+	  end,
+    {[{Fun,Desc}],[],[],[vertex]}.
+
+simple_check_selection(#st{sel=[{_,{1,{_,_,_}}}]}) ->
+   {none,""};
+simple_check_selection(#st{sel=Sel}) when Sel =/= [] ->
+   {none,?__(1,"You can move to only one point")};
+simple_check_selection(#st{sel=[]}) ->
+   {none,?__(2,"Nothing selected")}.
+
+simple_weld([{Obj,{1,{V,_,_}}}],#st{sel=Sel,shapes=Shs}=St) ->
+   We = gb_trees:get(Obj, Shs),
+   Vtab = We#we.vp,
+   Target = gb_trees:get(V, Vtab),
+   NewShs = do_move(Target,Sel,Shs),
+   {save_state,St#st{shapes=NewShs}}.
+
+do_move(_,[],Shs) ->
+   Shs;
+do_move(Target,[{Obj,Vset}|Rest],Shapes) ->
+   We = gb_trees:get(Obj, Shapes),
+   Vtab = We#we.vp,
+   NewVtab = move_object(Target,Vset,Vtab),
+   NewWe = We#we{vp=NewVtab},
+   NewShapes = gb_trees:update(Obj,NewWe,Shapes),
+   do_move(Target,Rest,NewShapes).
+
+move_object(Target,Vset,Vtab) ->
+   case gb_sets:size(Vset) of
+      0 -> Vtab;
+      _ -> 
+         {Vertex,Vset2} = gb_sets:take_smallest(Vset),
+         NewVtab = gb_trees:update(Vertex,Target,Vtab),
+         move_object(Target,Vset2,NewVtab)
+   end.
