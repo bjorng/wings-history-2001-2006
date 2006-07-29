b40fe36c00f330fd559d0d69015e727aa4cb24db
Anthony D'Agostino <antoneos@users.sourceforge.net>
2006-07-29 20:48:58 +0000

    Revived the Plane Plugin

--- plugins_src/primitives/wpc_plane.erl
+++ plugins_src/primitives/wpc_plane.erl
@@ -1,157 +1,169 @@
-%% Plane Plugin for Wings 0.90 or higher.
-%% Copyright (c) 2002 Anthony D'Agostino (scorpius@compuserve.com)
 %%
-%% For updates and other plugins:
-%% http://ourworld.compuserve.com/homepages/scorpius
+%%  wpc_plane.erl --
+%%
+%%     Plane Plugin (generates four classic types of planes)
+%%
+%%  Copyright (c) 2002-2006 Anthony D'Agostino
+%%
+%%  See the file "license.terms" for information on usage and redistribution
+%%  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+%%
+%%     $Id: wpc_plane.erl,v 1.3 2006/07/29 20:48:58 antoneos Exp $
+%%
 
 -module(wpc_plane).
 -export([init/0,menu/2,command/2]).
 -import(math, [sqrt/1,cos/1,sin/1,pi/0,pow/2,exp/1]).
+-include("wings_intl.hrl").
 
 init() -> true.
 
-menu({shape}, Menu0) ->
-	Menu0 ++ [separator,
-		{"Plane",plane,[option]},
-		{"Lumpy Plane",lumpyplane,[option]},
-		{"Wavy Plane",wavyplane,[option]},
-		{"Sombrero Plane",sombreroplane,[option]}
-		];
+menu({shape,more}, []) ->
+    plane_menu();
+menu({shape,more}, Menu) ->
+    Menu ++ [separator|plane_menu()];
 menu(_, Menu) -> Menu.
 
-command({shape,{plane,Ask}}, St) -> make_plane(Ask, St);
-command({shape,{lumpyplane,Ask}}, St) -> make_lumpyplane(Ask, St);
-command({shape,{wavyplane,Ask}}, St) -> make_wavyplane(Ask, St);
-command({shape,{sombreroplane,Ask}}, St) -> make_sombreroplane(Ask, St);
+plane_menu() ->
+    [{?__(1,"Regular Plane"),regularplane,[option]},
+     {?__(2,"Lumpy Plane"),lumpyplane,[option]},
+     {?__(3,"Wavy Plane"),wavyplane,[option]},
+     {?__(4,"Sombrero Plane"),sombreroplane,[option]}].
+
+command({shape,{more,{regularplane  ,Ask}}},_St) -> make_regular_plane(Ask);
+command({shape,{more,{lumpyplane    ,Ask}}},_St) -> make_lumpy_plane(Ask);
+command({shape,{more,{wavyplane     ,Ask}}},_St) -> make_wavy_plane(Ask);
+command({shape,{more,{sombreroplane ,Ask}}},_St) -> make_sombrero_plane(Ask);
 command(_, _) -> next.
 
 %%% The rest are local functions.
 
 % ======= Regular Plane =======
-make_plane(Ask, St) when is_atom(Ask) ->
-	wpa:ask(Ask, [ {"Resolution",5},
-				   {"Size",2}
-				 ],
-	St, fun(Res) -> {shape,{plane,Res}} end);
-make_plane([Nres, Size], St) ->
-	Vs = plane_verts(Nres, Size),
-	Fs = plane_faces_top(Nres, Nres)++plane_faces_bot(Nres, Nres),
-	{new_shape,"Plane",Fs,Vs}.
+make_regular_plane(Ask) when is_atom(Ask) ->
+    wpa:ask(Ask, ?__(1,"Create Regular Plane"),
+	    [{?__(2,"Resolution"),7},
+	     {?__(3,"Size"),2.0},
+	     {?__(4,"Thickness"), 0.1}],
+	    fun(Res) -> {shape,{more,{regularplane,Res}}} end);
+make_regular_plane([Nres, Size, Thickness]) ->
+    Vs = regular_plane_verts(Nres, Size, +Thickness/2) ++
+	 regular_plane_verts(Nres, Size, -Thickness/2),
+    Fs = plane_faces(Nres, Nres),
+    {new_shape,"Regular Plane",Fs,Vs}.
 
 % ======= Lumpy Plane =======
-make_lumpyplane(Ask, St) when is_atom(Ask) ->
-	wpa:ask(Ask, [ {"Resolution",30},
-				   {"Size",2},
-				   {"Lumps",2}
-				 ],
-	St, fun(Res) -> {shape,{lumpyplane,Res}} end);
-make_lumpyplane([Nres, Size, Lumps], St) ->
-	Vs = lumpyplane_verts(Nres, Size, Lumps),
-	Fs = plane_faces_top(Nres, Nres)++plane_faces_bot(Nres, Nres),
-	{new_shape,"Lumpy Plane",Fs,Vs}.
+make_lumpy_plane(Ask) when is_atom(Ask) ->
+    wpa:ask(Ask, ?__(1,"Create Lumpy Plane"),
+	    [{?__(2,"Resolution"),30},
+	     {?__(3,"Size"),2.0},
+	     {?__(4,"Lumps"),2},
+	     {?__(5,"Thickness"), 0.1}],
+	    fun(Res) -> {shape,{more,{lumpyplane,Res}}} end);
+make_lumpy_plane([Nres, Size, Lumps, Thickness]) ->
+    Vs = lumpy_plane_verts(Nres, Size, Lumps, +Thickness/2) ++
+	 lumpy_plane_verts(Nres, Size, Lumps, -Thickness/2),
+    Fs = plane_faces(Nres, Nres),
+    {new_shape,"Lumpy Plane",Fs,Vs}.
 
 % ======= Wavy Plane =======
-make_wavyplane(Ask, St) when is_atom(Ask) ->
-	wpa:ask(Ask, [ {"Resolution",60},
-				   {"Size",2},
-				   {"Waves",4},
-				   {"Height", 0.2}
-				 ],
-	St, fun(Res) -> {shape,{wavyplane,Res}} end);
-make_wavyplane([Nres, Size, Waves, Height], St) ->
-	Vs = wavyplane_verts(Nres, Size, Waves, Height),
-	Fs = plane_faces_top(Nres, Nres)++plane_faces_bot(Nres, Nres),
-	{new_shape,"Wavy Plane",Fs,Vs}.
+make_wavy_plane(Ask) when is_atom(Ask) ->
+    wpa:ask(Ask, ?__(1,"Make Wavy Plane"),
+	    [{?__(2,"Resolution"),60},
+	     {?__(3,"Size"),2.0},
+	     {?__(4,"Waves"),4},
+	     {?__(5,"Height"), 0.2},
+	     {?__(6,"Thickness"), 0.1}],
+	    fun(Res) -> {shape,{more,{wavyplane,Res}}} end);
+make_wavy_plane([Nres, Size, Waves, Height, Thickness]) ->
+    Vs = wavy_plane_verts(Nres, Size, Waves, Height, +Thickness/2) ++
+	 wavy_plane_verts(Nres, Size, Waves, Height, -Thickness/2),
+    Fs = plane_faces(Nres, Nres),
+    {new_shape,"Wavy Plane",Fs,Vs}.
 
 % ======= Sombrero Plane =======
-make_sombreroplane(Ask, St) when is_atom(Ask) ->
-	wpa:ask(Ask, [ {"Resolution",60},
-				   {"Size",2},
-				   {"Waves",4},
-				   {"Falloff",1},
-				   {"Height", 1}
-				 ],
-	St, fun(Res) -> {shape,{sombreroplane,Res}} end);
-make_sombreroplane([Nres, Size, Waves, Falloff, Height], St) ->
-        Vs = sombreroplane_verts(Nres, Size, Waves, Falloff, Height),
-        Fs = plane_faces_top(Nres, Nres)++plane_faces_bot(Nres, Nres),
-	{new_shape,"Sombrero Plane",Fs,Vs}.
+make_sombrero_plane(Ask) when is_atom(Ask) ->
+    wpa:ask(Ask, ?__(1,"Make Sombrero Plane"),
+	    [{?__(2,"Resolution"),60},
+	     {?__(3,"Size"),2.0},
+	     {?__(4,"Waves"),4},
+	     {?__(5,"Falloff"),1},
+	     {?__(6,"Height"), 1},
+	     {?__(7,"Thickness"), 0.1}],
+	    fun(Res) -> {shape,{more,{sombreroplane,Res}}} end);
+make_sombrero_plane([Nres, Size, Waves, Falloff, Height, Thickness]) ->
+    Vs = sombrero_plane_verts(Nres, Size, Waves, Falloff, Height, +Thickness/2) ++
+	 sombrero_plane_verts(Nres, Size, Waves, Falloff, Height, -Thickness/2),
+    Fs = plane_faces(Nres, Nres),
+    {new_shape,"Sombrero Plane",Fs,Vs}.
 
-% ======= Regular Plane: Generate Verts =======
-plane_verts(Nres, Size) ->
-	Verts = [[
-			{
-			Size*(I/(Nres-1.0)*2-1),
-			Size*(0.0),
-			Size*(J/(Nres-1.0)*2-1)
-			} % this tuple contains the x,y,z coordinates of this vertex
-			|| J <- lists:seq(0, Nres-1)]
-			|| I <- lists:seq(0, Nres-1)],
-	lists:append(Verts). % add it to the list of verts
+regular_plane_verts(Nres, Size, Thickness) ->
+    [{Size*(I/(Nres-1.0)*2-1),
+      Thickness,
+      Size*(J/(Nres-1.0)*2-1)}
+      || I <- lists:seq(0, Nres-1), J <- lists:seq(0, Nres-1)].
 
-% ======= Lumpy Plane: Generate Verts =======
-lumpyplane_verts(Nres, Size, Lumps) ->
-	Verts = [[
-			{
-			Size*(I/(Nres-1.0)*2-1),
-			Size*(cos(Lumps*(I/(Nres-1.0)*2-1)*pi())*
-				  cos(Lumps*(J/(Nres-1.0)*2-1)*pi())/(Lumps*3)),
-			Size*(J/(Nres-1.0)*2-1)
-			} % this tuple contains the x,y,z coordinates of this vertex
-			|| J <- lists:seq(0, Nres-1)]
-			|| I <- lists:seq(0, Nres-1)],
-	lists:append(Verts). % add it to the list of verts
+lumpy_plane_verts(Nres, Size, Lumps, Thickness) ->
+    [{Size*(I/(Nres-1.0)*2-1),
+      (Size*(cos(Lumps*(I/(Nres-1.0)*2-1)*pi())*
+	     cos(Lumps*(J/(Nres-1.0)*2-1)*pi())/(Lumps*3)))+Thickness,
+      Size*(J/(Nres-1.0)*2-1)}
+      || I <- lists:seq(0, Nres-1), J <- lists:seq(0, Nres-1)].
 
-% ======= Wavy Plane: Generate Verts =======
-wavyplane_verts(Nres, Size, Waves, Height) ->
-	Verts = [[
-			{
-			Size*(I/(Nres-1.0)*2-1),
-			Size*(1.0/Waves *
-				  cos(2*pi()*sqrt( pow((Waves*(I/(Nres-1.0)*2-1)), 2)
-								 + pow((Waves*(J/(Nres-1.0)*2-1)), 2))))*Height,
-			Size*(J/(Nres-1.0)*2-1)
-			} % this tuple contains the x,y,z coordinates of this vertex
-			|| J <- lists:seq(0, Nres-1)]
-			|| I <- lists:seq(0, Nres-1)],
-	lists:append(Verts). % add it to the list of verts
+wavy_plane_verts(Nres, Size, Waves, Height, Thickness) ->
+    [{Size*(I/(Nres-1.0)*2-1),
+      (Size*(1.0/Waves *
+	  cos(2*pi()*sqrt(pow((Waves*(I/(Nres-1.0)*2-1)), 2)
+			+ pow((Waves*(J/(Nres-1.0)*2-1)), 2))))*Height)+Thickness,
+      Size*(J/(Nres-1.0)*2-1)}
+      || I <- lists:seq(0, Nres-1), J <- lists:seq(0, Nres-1)].
 
-% ======= Sombrero Plane: Generate Verts =======
-sombreroplane_verts(Nres, Size, Waves, Falloff, Height) ->
-	Verts = [[
-			{
-			Size*(I/(Nres-1.0)*2-1),
-			Size*(1.0/Waves *
-				  cos(2*pi()*sqrt( pow((Waves*(I/(Nres-1.0)*2-1)), 2)
-								  +pow((Waves*(J/(Nres-1.0)*2-1)), 2)))
-					  * exp(-sqrt( pow((Waves*(I/(Nres-1.0)*2-1)), 2)
-								  +pow((Waves*(J/(Nres-1.0)*2-1)), 2))*Falloff))*Height,
-			Size*(J/(Nres-1.0)*2-1)
-			} % this tuple contains the x,y,z coordinates of this vertex
-			|| J <- lists:seq(0, Nres-1)]
-			|| I <- lists:seq(0, Nres-1)],
-	lists:append(Verts). % add it to the list of verts
+sombrero_plane_verts(Nres, Size, Waves, Falloff, Height, Thickness) ->
+    [{Size*(I/(Nres-1.0)*2-1),
+      (Size*(1.0/Waves *
+	  cos(2*pi()*sqrt(pow((Waves*(I/(Nres-1.0)*2-1)), 2)
+			 +pow((Waves*(J/(Nres-1.0)*2-1)), 2)))
+	      * exp(-sqrt(pow((Waves*(I/(Nres-1.0)*2-1)), 2)
+			 +pow((Waves*(J/(Nres-1.0)*2-1)), 2))*Falloff))*Height)+Thickness,
+      Size*(J/(Nres-1.0)*2-1)}
+      || I <- lists:seq(0, Nres-1), J <- lists:seq(0, Nres-1)].
 
+plane_faces(Nres, Nres) ->
+    plane_faces_top(Nres, Nres) ++
+    plane_faces_bot(Nres, Nres) ++
+    plane_faces_left(Nres, Nres) ++
+    plane_faces_right(Nres, Nres) ++
+    plane_faces_front(Nres, Nres) ++
+    plane_faces_back(Nres, Nres).
 
-% ======= Generate Faces For Plane =======
 plane_faces_top(Ures, Vres) ->
-	Faces = [[
-			[
-			I*Vres+J, I*Vres+J+1, (I+1)*Vres+J+1, (I+1)*Vres+J
-			] % this list contains the list of vertex indices
-			  % in this face (a quad)
-			|| J <- lists:seq(0, Vres-2)]
-			|| I <- lists:seq(0, Ures-2)],
-	lists:append(Faces). % add it to the list of faces
-plane_faces_bot(Ures, Vres) ->
-	Faces = [[
-			[
-			(I+1)*Vres+J, (I+1)*Vres+J+1, I*Vres+J+1, I*Vres+J
-			] % this list contains the list of vertex indices
-			  % in this face (a quad)
-			|| J <- lists:seq(0, Vres-2)]
-			|| I <- lists:seq(0, Ures-2)],
-	lists:append(Faces). % add it to the list of faces
+    [[I*Vres+J,I*Vres+J+1,(I+1)*Vres+J+1,(I+1)*Vres+J]
+      || I <- lists:seq(0, Vres-2), J <- lists:seq(0, Ures-2)].
 
-	% Is there a faster way to reverse a list of lists?
+plane_faces_bot(Ures, Vres) ->
+    Offset = Ures*Vres,
+    [[Offset + ((I+1)*Vres+J),
+      Offset + ((I+1)*Vres+J+1),
+      Offset + (I*Vres+J+1),
+      Offset + (I*Vres+J)]
+      || I <- lists:seq(0, Vres-2), J <- lists:seq(0, Ures-2)].
+
+plane_faces_left(Ures, Vres) ->
+    A = lists:seq(0, Vres-1),
+    B = lists:map(fun(X) -> (Ures*Vres)+X end, A),
+    [lists:reverse(A) ++ B].
+
+plane_faces_right(Ures, Vres) ->
+    A = lists:seq(Ures*Vres-1, Ures*Vres-Vres, -1),
+    B = lists:map(fun(X) -> (Ures*Vres)+X end, A),
+    [lists:reverse(A) ++ B].
+
+plane_faces_front(Ures, Vres) ->
+    A = lists:seq(Ures*Vres-Vres, 0, -Vres),
+    B = lists:map(fun(X) -> (Ures*Vres)+X end, A),
+    [lists:reverse(A) ++ B].
+
+plane_faces_back(Ures, Vres) ->
+    A = lists:seq(Vres-1, Ures*Vres-1, Vres),
+    B = lists:map(fun(X) -> (Ures*Vres)+X end, A),
+    [lists:reverse(A) ++ B].
 
